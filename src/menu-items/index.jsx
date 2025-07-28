c13bc5b8a81d63e154f12e53bb587b4f6af94912
brownie-au <robbrown.au@gmail.com>
2025-07-28 14:28:45 +1000

    chore: initial import of Datta Able React template

---
+++ src/menu-items/index.jsx
@@ -0,0 +1,18 @@
+// project-imports
+import application from './application';
+import adminPanel from './admin-panel';
+import chartsMaps from './charts-maps';
+import formComponents from './forms';
+import other from './other';
+import pages from './pages';
+import uiComponents from './ui-components';
+import tableRoutes from './tables';
+import navigation from './navigation';
+
+// ==============================|| MENU ITEMS ||============================== //
+
+const menuItems = {
+  items: [navigation, adminPanel, uiComponents, formComponents, tableRoutes, chartsMaps, application, pages, other]
+};
+
+export default menuItems;
