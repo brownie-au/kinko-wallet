a4bb5f59345e94b9f81cceaed94a2a46cd476917
brownie-au <robbrown.au@gmail.com>
2025-09-03 16:02:40 +1000

    Update: PulseChain Scan API integration

--- api/ping.js
+++ api/ping.js
@@ -2,5 +2,5 @@
 export default function handler(req, res) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-  res.end('pong gong');
+  res.end('pong wong');
 }
